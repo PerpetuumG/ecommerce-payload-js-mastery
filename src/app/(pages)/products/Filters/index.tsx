7556aa429a915a02c44e57564178ddc303911098
PerpetuumG <perpetuumg@gmail.com>
2023-12-30 08:55:41 +0200

    Products

---
+++ src/app/(pages)/products/Filters/index.tsx
@@ -0,0 +1,8 @@
+import { Category } from '../../../../payload/payload-types';
+import classes from './index.module.scss';
+
+const Filters = ({ categories }: { categories: Category[] }) => {
+  return <div>Testing</div>;
+};
+
+export default Filters;
