ffcc034e6b19ba4d9c074bed02b2bb31ce479f21
PerpetuumG <perpetuumg@gmail.com>
2024-01-10 14:16:55 +0200

    Order Confirmation Page

--- src/app/(pages)/order-confirmation/OrderConfirmationPage/index.tsx
+++ src/app/(pages)/order-confirmation/OrderConfirmationPage/index.tsx
@@ -1,24 +1,24 @@
-'use client'
+'use client';
 
-import React, { Fragment, useEffect } from 'react'
-import { useSearchParams } from 'next/navigation'
+import React, { Fragment, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
 
-import { Button } from '../../../_components/Button'
-import { Message } from '../../../_components/Message'
-import { useCart } from '../../../_providers/Cart'
+import { Button } from '../../../_components/Button';
+import { Message } from '../../../_components/Message';
+import { useCart } from '../../../_providers/Cart';
 
-import classes from './index.module.scss'
+import classes from './index.module.scss';
 
 export const OrderConfirmationPage: React.FC<{}> = () => {
-  const searchParams = useSearchParams()
-  const orderID = searchParams.get('order_id')
-  const error = searchParams.get('error')
+  const searchParams = useSearchParams();
+  const orderID = searchParams.get('order_id');
+  const error = searchParams.get('error');
 
-  const { clearCart } = useCart()
+  const { clearCart } = useCart();
 
   useEffect(() => {
-    clearCart()
-  }, [clearCart])
+    clearCart();
+  }, [clearCart]);
 
   return (
     <div>
@@ -29,11 +29,11 @@ export const OrderConfirmationPage: React.FC<{}> = () => {
             {`Your payment was successful but there was an error processing your order. Please contact us to resolve this issue.`}
           </p>
           <div className={classes.actions}>
-            <Button href="/account" label="View account" appearance="primary" />
+            <Button href='/account' label='View account' appearance='primary' />
             <Button
               href={`${process.env.NEXT_PUBLIC_SERVER_URL}/orders`}
-              label="View all orders"
-              appearance="secondary"
+              label='View all orders'
+              appearance='secondary'
             />
           </div>
         </Fragment>
@@ -44,15 +44,15 @@ export const OrderConfirmationPage: React.FC<{}> = () => {
             {`Your order has been confirmed. You will receive an email confirmation shortly. Your order ID is ${orderID}.`}
           </p>
           <div className={classes.actions}>
-            <Button href={`/orders/${orderID}`} label="View order" appearance="primary" />
+            <Button href={`/account/orders/${orderID}`} label='View order' appearance='primary' />
             <Button
-              href={`${process.env.NEXT_PUBLIC_SERVER_URL}/orders`}
-              label="View all orders"
-              appearance="secondary"
+              href={`${process.env.NEXT_PUBLIC_SERVER_URL}/account/orders`}
+              label='View all orders'
+              appearance='secondary'
             />
           </div>
         </Fragment>
       )}
     </div>
-  )
-}
+  );
+};
